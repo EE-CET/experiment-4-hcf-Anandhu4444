c80a9f9617a3dc188c7edcbe1f3567ab055657e5
Anandhu4444 <hey4444567889987@gmail.com>
2026-01-31 16:48:24 +0530

    Update HCF.java

--- src/HCF.java
+++ src/HCF.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
 public class HCF {
-    
-        // TODO: Read two integers and print their HCF
-    
+        public static int greatest(int a,int b){
+	while(b!=0){
+	        int temp=b;
+		b=a%b;
+		a=temp;
+	}
+	return a;
+
 }
+
+public static void main(String args[]){
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter two numbers");
+	int a=sc.nextInt();
+	int b=sc.nextInt();
+	System.out.println("HCF is "+greatest(a,b));
+    }
+}
+
+
